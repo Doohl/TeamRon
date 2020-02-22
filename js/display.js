89d541a67e2605bd864b72d001ff3cf6f1a38ffe
Doohl <dhlserrano@gmail.com>
2020-02-22 01:36:22 -0800

    Continued work on the webite

---
+++ js/display.js
@@ -0,0 +1,20 @@
+const mockData = {
+    'A': [
+        [1, 1, true],
+        [1, 2, true],
+        [1, 3, true],
+        [1, 4, true],
+        [1, 5, true],
+        [2, 1, true],
+        [2, 2, true],
+        [2, 3, true],
+        [2, 4, true],
+        [2, 5, true]
+    ],
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+    
+
+});
